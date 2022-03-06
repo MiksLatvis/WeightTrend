a7b86f36a3b64f2234c239c35dae1c778135dff4
Miks Latvis <miks.latvis@gmail.com>
2022-03-06 19:49:19 +0900

    Initial Commit

---
+++ WeightTrend/WeightTrendApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightTrendApp.swift
+//  WeightTrend
+//
+//  Created by Miks Latvis on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeightTrendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
